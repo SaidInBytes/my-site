4fd969cb48019022a9a02357115ee58f1e4a01e7
smokiinape <amanblvck@gmail.com>
2025-06-24 00:50:20 +0200

    Add copyright notice to footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="mt-auto w-full text-center py-4 text-gray-500">
       &copy; {new Date().getFullYear()}
+      <span className="ml-2">SaidInBytes</span>
+      
     </footer>
   );
 };
